4d3b9953c789213c077fb68b55567a37415ac104
rainot <olga24@onet.pl>
2024-10-18 22:40:18 +0200

    Add 4 missing plots

--- KomunikacjaCzlowiekKomputer/KomunikacjaCzlowiekKomputer.py
+++ KomunikacjaCzlowiekKomputer/KomunikacjaCzlowiekKomputer.py
@@ -23,12 +23,34 @@ def preparing_arrays(file_name, file_len):
 file_2cel = np.genfromtxt("2cel.csv", delimiter = ",")
 data_2cel = file_2cel[1:]
 len_2cel = len(data_2cel)
-
-
 xaxis_2cel, yaxis_2cel = preparing_arrays(file_2cel, len_2cel)
 
+file_cel = np.genfromtxt("cel.csv", delimiter = ",")
+data_cel = file_cel[1:]
+len_cel = len(data_cel)
+xaxis_cel, yaxis_cel = preparing_arrays(file_cel, len_cel)
 
-plt.plot(xaxis_2cel, yaxis_2cel, color="red")
+file_celrs = np.genfromtxt("cel-rs.csv", delimiter = ",")
+data_celrs = file_celrs[1:]
+len_celrs = len(data_celrs)
+xaxis_celrs, yaxis_celrs = preparing_arrays(file_celrs, len_celrs)
+
+file_2celrs = np.genfromtxt("2cel-rs.csv", delimiter = ",")
+data_2celrs = file_2celrs[1:]
+len_2celrs = len(data_2celrs)
+xaxis_2celrs, yaxis_2celrs = preparing_arrays(file_2celrs, len_2celrs)
+
+file_rsel = np.genfromtxt("rsel.csv", delimiter = ",")
+data_rsel = file_rsel[1:]
+len_rsel = len(data_rsel)
+xaxis_rsel, yaxis_rsel = preparing_arrays(file_rsel, len_rsel)
+
+
+plt.plot(xaxis_2cel, yaxis_2cel, color="magenta")
+plt.plot(xaxis_cel, yaxis_cel, color="black")
+plt.plot(xaxis_celrs, yaxis_celrs, color="green")
+plt.plot(xaxis_2celrs, yaxis_2celrs, color="red")
+plt.plot(xaxis_rsel, yaxis_rsel, color="blue")
 plt.xlabel("Rozegranych gier")
 plt.ylabel("Odsetek wygranych gier")
 
